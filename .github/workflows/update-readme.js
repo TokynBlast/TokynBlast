7e75371c44ef4700c1f911275bf104a9726244bb
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-22 14:22:28 -0800

    Added more images

--- .github/workflows/update-readme.js
+++ .github/workflows/update-readme.js
@@ -6,13 +6,16 @@ const today = format(new Date(), 'MM-dd');
 const readmePath = path.join(__dirname, 'README.md');
 const defaultImage = 'images/NUXtocat.gif';
 const dateRanges = [
-  { start: '10-24', end: '11-03', image: 'images/grim-repo.jpg' },
-  { start: '12-18', end: '12-28', image: 'images/saint-nicktocat.jpg' },
-  { start: '12-17', end: '12-17', image: 'images/homercat.png' },
-  { start: '05-05', end: '05-05', image: 'images/adventure-cat.png' },
-  { start: '06-01', end: '06-30', image: 'images/Octoqueer.png' },
-  { start: '10-10', end: '10-10', image: 'images/ten.jpg' },
-  { start: '01-23', end: '01-23', image: 'images/ten.jpg' }, // To test if it works!
+  { start: '10-24', end: '11-03', image: 'images/grim-repo.jpg' }, // Halloween
+  { start: '12-18', end: '12-28', image: 'images/saint-nicktocat.jpg' }, // Christmas
+  { start: '12-17', end: '12-17', image: 'images/homercat.png' }, // Simpsons air
+  { start: '05-05', end: '05-05', image: 'images/adventure-cat.png' }, // Adventure time airs
+  { start: '06-01', end: '06-30', image: 'images/Octoqueer.png' }, // LGBTQ+ month :)
+  { start: '10-10', end: '10-10', image: 'images/ten.jpg' }, // Tenth day of the tenth month
+  { start: '5-11', end: '5-11', image: 'images/momtocat.png' }, // Mothers day (2025)
+  { start: '5-11', end: '5-11', image: 'images/seteve.jpg' }, // Steve Jobs dies :(
+  { start: '5-11', end: '5-11', image: 'images/day-of-dead.jpg' }, // Day of the dead
+  { start: '01-23', end: '01-24', image: 'images/ten.jpg' } // To test if it works!
 ];
 
 // Determine the correct image
